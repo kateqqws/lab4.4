1fd96d3fca5c7f2db782da105ddcb5cad0ba0135
unknown <katepob160@gmail.com>
2023-10-12 22:50:14 +0300

    upload files

---
+++ лаб4.4/лаб4.4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double xStart;
+	double xEnd;
+	double dx;
+	double R;
+	double y;
+	cout << "R = "; cin >> R;
+	cout << "x Start = "; cin >> xStart;
+	cout << "x End = "; cin >> xEnd;
+	cout << "dx = "; cin >> dx;
+
+	cout << "---------------------------" << endl;
+	cout << "|    " << "x" << "    |    "
+		 << "y" << "    |" << endl;
+	for (int x = xStart; x <= xEnd; x += dx)
+	{
+		if (x <= 0)
+			y = -(x / 2);
+		else
+			if (0 < x && x <= R)
+				y = R - sqrt(R * R - x * x);
+			else
+				if (R < x && x <= 2 * R)
+					y = sqrt(R * R - (x - R) * (x - R));
+				else
+					y = -(x - 2 * R) / (6 - 2 * R);
+
+		cout << "---------------------------" << endl;
+		cout << "|    " << x << "    |    "
+			<< y << "    |" << endl;
+	}
+	cout << "---------------------------" << endl;
+
+
+}
